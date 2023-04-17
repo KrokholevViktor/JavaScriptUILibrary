20e263ef4ff7588b97a8ea2c7f7bb545d292bf85
xxxrePAPRIKA <xxxre.paprika@gmail.com>
2023-04-17 14:20:27 +0700

    carousel

--- src/js/lib/components/accordion.js
+++ src/js/lib/components/accordion.js
@@ -1,17 +1,18 @@
-import $ from "../core";
+import $ from '../core';
 
 $.prototype.accordion = function(headActive = 'accordion-head--active', contentActive = 'accordion-content--active', paddings = 40) {
     for (let i = 0; i < this.length; i++) {
         $(this[i]).click(() => {
             $(this[i]).toggleClass(headActive);
             $(this[i].nextElementSibling).toggleClass(contentActive);
+
             if (this[i].classList.contains(headActive)) {
-                this[i].nextElementSibling.style.maxHeigh = this[i].nextElementSibling.scrollHeigh + paddings + 'px'
+                this[i].nextElementSibling.style.maxHeight = this[i].nextElementSibling.scrollHeight + paddings + "px";
             } else {
-                this[i].nextElementSibling.style.maxHeigh = '0px';
+                this[i].nextElementSibling.style.maxHeight = "0px";
             }
-        })
+        });
     }
 };
 
-$('.acctordion-head').accordion();
+$('.accordion-head').accordion();
